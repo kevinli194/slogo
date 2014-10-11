e04521412c5414c6446493cfc2929d8f61158f2f
Sandy Lee <sl269@duke.edu>
2014-10-10 23:55:44 -0400

    added tursome turtle and other instructions :)

--- src/Instructions/ListInstruction.java
+++ src/Instructions/ListInstruction.java
@@ -1,31 +1,39 @@
 package Instructions;
 
-import java.util.Stack;
-
+import java.util.ArrayList;
+import java.util.List;
 import model.ObservableData;
 
+
 public class ListInstruction implements Instruction {
+    private List<Instruction> myInstructionList = new ArrayList<Instruction>();
 
-	public ListInstruction(Stack<Instruction> s) {
+    public ListInstruction (List<Instruction> lst) {
+        myInstructionList = lst;
 
-	}
+    }
 
-	@Override
-	public double execute(ObservableData od) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public double execute (ObservableData od) {
+        // TODO Auto-generated method stub
+        double returnAmt = 0;
+        for(Instruction instr : myInstructionList){
+            returnAmt = instr.execute(od);
+        }
+            
+        return returnAmt;
+       
+    }
 
-	@Override
-	public int getNumParams() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getNumParams () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void addParam(Instruction param) {
-		// TODO Auto-generated method stub
-
-	}
+    @Override
+    public void addParam (Instruction param) {
+        // TODO Auto-generated method stub
+    }
 
 }
--- src/parser/InstructionFactory.java
+++ src/parser/InstructionFactory.java
@@ -5,10 +5,18 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.regex.Pattern;
-
 import Instructions.ConstantInstruction;
 import Instructions.Instruction;
+import Instructions.TurtleCommand.Back;
 import Instructions.TurtleCommand.Forward;
+import Instructions.TurtleCommand.If;
+import Instructions.TurtleCommand.Left;
+import Instructions.TurtleCommand.Less;
+import Instructions.TurtleCommand.Repeat;
+import Instructions.TurtleCommand.Right;
+import Instructions.TurtleCommand.SetHeading;
+import Instructions.TurtleCommand.Sum;
+import Instructions.TurtleCommand.Towards;
 
 /**
  * Factory design pattern for creating 
@@ -59,6 +67,46 @@ public class InstructionFactory {
 			System.out.println("forward!");
 			return new Forward();
 		}
+		else if (type.equals("back")) {
+                    System.out.println("back!");
+                    return new Back();
+            }
+		else if (type.equals("repeat")) {
+                    System.out.println("repeat!");
+                    return new Repeat();
+            }
+		else if (type.equals("if")) {
+                    System.out.println("if!");
+                    return new If();
+		}
+		else if (type.equals("sum")) {
+                    System.out.println("sum!");
+                    return new Sum();
+                }
+		else if (type.equals("right")) {
+                    System.out.println("right!");
+                    return new Right();
+                }
+		else if (type.equals("left")) {
+                    System.out.println("left!");
+                    return new Left();
+                }
+		else if (type.equals("sum")) {
+                    System.out.println("sum!");
+                    return new Sum();
+                }
+		else if (type.equals("less")) {
+                    System.out.println("less!");
+                    return new Less();
+                }
+		else if (type.equals("setheading")) {
+                    System.out.println("setheading!");
+                    return new SetHeading();
+                }
+		else if (type.equals("towards")) {
+                    System.out.println("Towards!");
+                    return new Towards();
+                }
 		else {
 			return null;
 		}
