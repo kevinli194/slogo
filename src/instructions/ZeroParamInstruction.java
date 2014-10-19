95f8ebf25ec4d6970437c38a81c22d593addb04c
Jennie Ju <jsj18@duke.edu>
2014-10-19 14:39:51 -0400

    Merged with master

---
+++ src/instructions/ZeroParamInstruction.java
@@ -0,0 +1,26 @@
+package instructions;
+
+/**
+ * Skeleton abstract class for commands
+ * that have no parameters to execute on
+ * @author slogo_team02 a.k.a. TEAM ROCKET
+ *
+ */
+public abstract class ZeroParamInstruction implements Instruction {
+
+	/**
+	 * Needs zero outside parameters
+	 */
+	@Override
+	public int getNumParams() {
+		return 0;
+	}
+
+	/**
+	 * Does nothing when asked to add parameters
+	 */
+	@Override
+	public void addParam(Instruction param) {
+	}
+
+}
